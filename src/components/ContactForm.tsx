232c56aa9e9a5acb95f50fbdac5657ad2da5197f
SEANDOUGHTYEDL <sean.doughty@mail.utoronto.ca>
2020-12-07 18:08:27 -0500

    feat: adding EmailJS to contact

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -1,23 +1,44 @@
 import React from 'react';
+import emailjs from 'emailjs-com';
 
-const ContactForm = () => (
+declare var process : {
+  env: {
+    SERVICE_ID: string
+    TEMPLATE_ID: string
+    USER_ID: string
+  }
+}
+
+const ContactForm = () => {
+  function sendEmail(e: any) {
+    e.preventDefault();
+
+    emailjs.sendForm(process.env["SERVICE_ID"], process.env["TEMPLATE_ID"], e.target, process.env["USER_ID"])
+      .then((result) => {
+        console.log(result.text);
+      }, (error) => {
+        console.log(error.text);
+      });
+  }
+  
+  return (
     <div className="form-block">
-    <form>
-      <div className="form-group">
-      <label>Name</label>
-      <input type="text" className="form-control" id="InputName" placeholder="Enter name" />
-      </div>
-      <div className="form-group">
-      <label>Email address</label>
-      <input type="email" className="form-control" id="InputEmail" placeholder="Enter email" />
-      </div>
-      <div className="form-group">
-      <label>Message</label>
-            <textarea className="form-control" id="exampleFormControlTextarea1" rows={5}></textarea>
-      </div>
-      <button type="submit" className="btn btn-info btn-md">Send Message</button>
-    </form>
+      <form className="contact-form" onSubmit={sendEmail}>
+        <div className="form-group">
+          <label>Name</label>
+          <input type="text" name="from_name" className="form-control" placeholder="Enter name" />
+        </div>
+        <div className="form-group">
+          <label>Email address</label>
+          <input type="email" name="reply_to" className="form-control" placeholder="Enter email" />
+        </div>
+        <div className="form-group">
+          <label>Message</label>
+          <textarea name="message" className="form-control" id="exampleFormControlTextarea1" rows={5}></textarea>
+        </div>
+        <button type="submit" value="Send" className="btn btn-info btn-md">Send Message</button>
+      </form>
     </div>
   );
-
+}
 export default ContactForm;
