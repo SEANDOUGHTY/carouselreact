a1c67f91ad3060040e9574a09899d3691a7853ba
SEANDOUGHTYEDL <sean.doughty@mail.utoronto.ca>
2020-12-08 20:24:00 -0500

    changing style on header and carousel

--- src/components/CarouselItem.tsx
+++ src/components/CarouselItem.tsx
@@ -4,11 +4,11 @@ const CarouselItem = (props: any) => {
   return (
     <div className={`carousel-item py-3 ${props.active === "true" ? "active" : ""}`}>
         <div className="row carousel-row">
-          <div className="col-md-5 carousel-image-caption">
+          <div className="col-md-5 carousel-image-caption my-auto">
             <h1>{props.title}</h1>
             <h2>{props.body}</h2>
             <br />
-            <a className="btn btn-default mt-4" href="/#purchase" role="button">{props.button}</a>
+            <a className="btn btn-default border-warning mt-4" href="/#purchase" role="button">{props.button}</a>
           </div>
           <div className="col-md-7 corousel-image-col text-center">
             <img
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,6 +6,7 @@ const Header = (data: any) => (
     <div className="nav-logo">
     <a href="/#"><img src={image} alt="logo"/></a>
     </div>
+    <div className="navbar-border">
   <nav className="navbar navbar-expand-md navbar-light bg-light">
   <button className="navbar-toggler ml-auto" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon"></span>
@@ -13,14 +14,15 @@ const Header = (data: any) => (
   <div className="collapse navbar-collapse text-right" id="navbarTogglerDemo01">
     <ul className="navbar-nav navbar-right">
       <li className="nav-item active">
-      <a className="btn navbar-btn btn-default py-1 mx-1" href="/#">خانه<span className="sr-only">(current)</span></a>
+      <a className="btn navbar-btn btn-default border-warning py-1 mx-1" href="/#">خانه<span className="sr-only">(current)</span></a>
       </li>
       <li className="nav-item">
-      <a className="btn navbar-btn btn-default py-1 mx-1" href="/contact">{data.info.contactTitle}</a>
+      <a className="btn navbar-btn btn-default border-warning py-1 mx-1" href="/contact">{data.info.contactTitle}</a>
       </li>
     </ul>
   </div>  
-</nav>
+    </nav>
+    </div>
 </header>
 );
 
